95a26ce5c51687b301439dfa53835a8aae36680b
sinskiy <sinskiy@proton.me>
2024-06-08 18:56:46 +0300

    solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  const inCelcius = ((temperature - 32) * 5) / 9;
+  const rounded =
+    inCelcius !== Math.floor(inCelcius)
+      ? parseFloat(inCelcius.toFixed(1))
+      : inCelcius;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  const inFahrenheit = (temperature * 9) / 5 + 32;
+  const rounded =
+    inFahrenheit !== Math.floor(inFahrenheit)
+      ? parseFloat(inFahrenheit.toFixed(1))
+      : inFahrenheit;
+  return rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
