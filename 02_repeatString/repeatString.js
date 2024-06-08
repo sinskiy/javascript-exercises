efeafc50b41556e36bfb4106444e3cd8fa1bd25d
sinskiy <sinskiy@proton.me>
2024-06-08 17:44:53 +0300

    finish the second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+  if (number < 0) return "ERROR";
 
+  return string.repeat(number);
 };
 
 // Do not edit below this line
